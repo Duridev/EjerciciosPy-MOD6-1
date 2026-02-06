531dc0732816a7b24fdad4958e2c96b77a19accd
duridev <duridev@outlook.com>
2026-02-05 21:42:49 -0300

    add members app with views, templates, and URL routing

--- my_first_project/members/views.py
+++ my_first_project/members/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def members(request):
+    contexto ={
+        'curso': 'Django para Principiantes',
+        'duracion': 40,
+        'nivel': 'Básico'
+    }
+    return render(request, 'miprimer.html', contexto)
